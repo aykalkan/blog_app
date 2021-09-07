5d0f1b50762d4db0a56c15aeba69ec9ff52181c1
Ahmet Yasin Kalkanlı <ahyakal@gmail.com>
2021-09-07 14:24:11 +0300

    Flutter_statusbar_manager paketi yüklendi

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_statusbar_manager/flutter_statusbar_manager.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    FlutterStatusbarManager.setColor(Colors.transparent);    
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text("Latest Feed"),
+        actions: [
+          FloatingActionButton(
+            onPressed: () {
+              return null;
+            },
+          ),
+        ],
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) => Text("item $index"),
+      ),
     );
   }
 }
