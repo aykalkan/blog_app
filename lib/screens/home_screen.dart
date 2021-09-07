e2999ed5968ec36fb68a358a9904fc7880d4281e
Ahmet Yasin Kalkanlı <ahyakal@gmail.com>
2021-09-07 14:51:37 +0300

    Appbar ve statusbar düzenlendi

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,24 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_statusbar_manager/flutter_statusbar_manager.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    FlutterStatusbarManager.setColor(Colors.transparent);    
     return Scaffold(
       appBar: AppBar(
-        title: Text("Latest Feed"),
+        title: Text(
+          "Latest Feed",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
         actions: [
           FloatingActionButton(
+            child: Icon(Icons.add),
             onPressed: () {
               return null;
             },
+            elevation: 8,
+            mini: true,
           ),
         ],
+        toolbarHeight: 100,
         backgroundColor: Colors.transparent,
         elevation: 0,
+        backwardsCompatibility: false,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+        ),
       ),
       body: ListView.builder(
         itemCount: 10,
