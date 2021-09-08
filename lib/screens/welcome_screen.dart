c1ead8fc185ba36aa853aad0a5f9e9812214f0c4
Ahmet Yasin Kalkanlı <ahyakal@gmail.com>
2021-09-08 17:00:03 +0300

    Açılış ekranı temel tasarımı tamamlandı

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,5 +1,4 @@
-import 'dart:ui';
-
+import 'package:blog_app/controllers/welcome_image_dots.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatelessWidget {
@@ -26,7 +25,90 @@ class WelcomeScreen extends StatelessWidget {
                   Colors.transparent,
                   Colors.black,
                 ],
-                stops: [ 0.7, 1.0],
+                stops: [0.6, 1.0],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Text(
+                    "The Future of Photography & Unsplash",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline3!
+                        .copyWith(color: Colors.white),
+                  ),
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUetqWKxw_yKx_rG80j-rBwapn-dmxVedugw&usqp=CAU",
+                      ),
+                    ),
+                    title: Text(
+                      "Tobias Van",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(horizontal: 0),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      WelcomeImageDots(isSelected: true),
+                      WelcomeImageDots(),
+                      WelcomeImageDots(),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          height: 70,
+                          padding: EdgeInsets.all(8),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              return null;
+                            },
+                            child: Text(
+                              "LOGIN",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .button!
+                                  .copyWith(fontSize: 16),
+                            ),
+                            style:
+                                ElevatedButton.styleFrom(primary: Colors.white),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 70,
+                          padding: EdgeInsets.all(8),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              return null;
+                            },
+                            child: Text(
+                              "CREATE ACCOUNT",
+                              style:
+                                  Theme.of(context).textTheme.button!.copyWith(
+                                        fontSize: 16,
+                                        color: Colors.white,
+                                      ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
               ),
             ),
           ),
