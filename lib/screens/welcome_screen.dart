d59209c5bf4a30f6fc19b8a24261b581d308d1d0
Ahmet Yasin Kalkanlı <ahyakal@gmail.com>
2021-09-08 17:12:55 +0300

    Açılıştaki buton logine yönlendirildi

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,5 +1,7 @@
 import 'package:blog_app/controllers/welcome_image_dots.dart';
+import 'package:blog_app/screens/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
@@ -73,7 +75,7 @@ class WelcomeScreen extends StatelessWidget {
                           padding: EdgeInsets.all(8),
                           child: ElevatedButton(
                             onPressed: () {
-                              return null;
+                              Get.to(() => LoginScreen());
                             },
                             child: Text(
                               "LOGIN",
