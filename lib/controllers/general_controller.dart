a0020beb59f5bfa8e16ba4882c943e2738a0506f
Ahmet Yasin Kalkanlı <ahyakal@gmail.com>
2021-09-09 15:09:57 +0300

    Controllera post listesi eklendi

--- lib/controllers/general_controller.dart
+++ lib/controllers/general_controller.dart
@@ -1,5 +1,13 @@
+import 'dart:html';
+
+import 'package:blog_app/models/post.dart';
 import 'package:get/get.dart';
 
 class GeneralController extends GetxController {
+  RxList<Post> _posts = <Post>[].obs;
+
+  get posts => _posts;
+
+  set posts(value) => _posts.value = value;
   
 }
