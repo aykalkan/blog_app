667edc4382d7c3544492ef8147480500e54458f9
Ahmet Yasin Kalkanlı <ahyakal@gmail.com>
2021-09-10 10:25:17 +0300

    Post controllerda add metodu oluşturuldu

--- lib/services/posts_service.dart
+++ lib/services/posts_service.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/models/post.dart';
 import 'package:blog_app/services/collection_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -5,8 +6,10 @@ class PostsService {
   final _collectionService = CollectionService(Collection.posts);
 
   get referance => _collectionService.referance;
+
   Stream<QuerySnapshot<Object?>> get docsStream =>
       _collectionService.getDocsStream();
+
   void postsStream() {
     docsStream.map(
       (event) {
@@ -15,5 +18,10 @@ class PostsService {
         });
       },
     );
+  }
+
+  void add(Post post) async {
+    final document = await _collectionService.add(post.toMap());
+    post.id = document.id;
   }
 }
