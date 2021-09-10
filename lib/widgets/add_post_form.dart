47f6df87763e3ca7344fd03525fec285413ba3fc
Ahmet Yasin Kalkanlı <ahyakal@gmail.com>
2021-09-10 09:48:40 +0300

    Form elemanları tamamlandı

--- lib/widgets/add_post_form.dart
+++ lib/widgets/add_post_form.dart
@@ -1,5 +1,7 @@
 import 'package:blog_app/screens/home_screen.dart';
+import 'package:blog_app/widgets/categories_dropdown.dart';
 import 'package:blog_app/widgets/chubby_elevated_button.dart';
+import 'package:blog_app/widgets/numeric_step_button.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -15,23 +17,27 @@ class AddPostForm extends StatelessWidget {
           children: [
             TextFormField(
               decoration: InputDecoration(
-                labelText: "Full Name",
+                labelText: "Title",
               ),
             ),
             TextFormField(
               decoration: InputDecoration(
-                labelText: "Email",
+                labelText: "Image URL",
               ),
             ),
+            CategoriesDropdown(),
+            NumericStepButton(onChanged: (value) => null),
             TextFormField(
               decoration: InputDecoration(
-                labelText: "Create Password",
+                labelText: "Content",
               ),
             ),
             ChubbyElevatedButton(
-              "CREATE ACCOUNT",
+              "ADD POST",
               margin: EdgeInsets.only(top: 32),
-              onPressed: () => Get.offAll(() => HomeScreen()),
+              onPressed: () {
+                Get.offAll(() => HomeScreen());
+              },
             ),
           ],
         ),
