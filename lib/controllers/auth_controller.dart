e4b908833014f5cfef83f91fcfeaa7e92cde3fff
Ahmet Yasin Kalkanlı <ahyakal@gmail.com>
2021-09-15 10:03:45 +0300

    Kullanıcı girişi kontrollörü yapıldı

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,8 +1,11 @@
+import 'package:blog_app/services/collection_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:blog_app/models/user.dart' as u;
 
 class AuthController extends GetxController {
   FirebaseAuth _auth = FirebaseAuth.instance;
+  CollectionService _usersCollection = CollectionService(Collection.users);
   late Rx<User?> _firebaseUser;
 
   get user => _firebaseUser.value;
@@ -15,7 +18,20 @@ class AuthController extends GetxController {
 
   void createUser(String name, String email, String password) async {
     try {
-      _auth.createUserWithEmailAndPassword(email: email, password: password);
+      final fUser = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      final user = u.User(
+        email: email,
+        name: name,
+      );
+
+      _usersCollection.addWithId(
+        fUser.user!.uid,
+        user.toJson(),
+      );
     } catch (e) {
       Get.snackbar("Sign up failed!", e.toString(),
           snackPosition: SnackPosition.BOTTOM);
@@ -24,7 +40,10 @@ class AuthController extends GetxController {
 
   void login(String email, String password) async {
     try {
-      _auth.signInWithEmailAndPassword(email: email, password: password);
+      _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
     } catch (e) {
       Get.snackbar("Login failed!", e.toString(),
           snackPosition: SnackPosition.BOTTOM);
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,14 +2,25 @@ class User {
   User({
     required this.name,
     required this.email,
-    this.photo,
+    this.photoUrl =
+        "https://pbs.twimg.com/profile_images/1371947593979334662/bjzvbLb2_400x400.jpg",
     this.posts = const [],
     this.favouritePosts = const [],
   });
 
   final String name;
   final String email;
-  final String? photo;
+  final String photoUrl;
   final List<String> posts;
   final List<String> favouritePosts;
+
+  Map<String, dynamic> toJson() {
+    return {
+      "name": name,
+      "email": email,
+      "photoUrl": photoUrl,
+      "posts": posts,
+      "favouritePosts": favouritePosts,
+    };
+  }
 }
